2b730bdfaa9dca9d5451880ccf1914f3b3d7de78
Maciej Mierzwiak <maciej.mierzwiak@astozi.pl>
2024-04-09 19:11:24 +0200

    Implementing Toasting

--- app/src/main/java/com/example/ngrfitness/Register.java
+++ app/src/main/java/com/example/ngrfitness/Register.java
@@ -1,6 +1,10 @@
 package com.example.ngrfitness;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Register extends AppCompatActivity {
+
+    TextInputEditText editTextEmail, editTextPassword;
+    Button buttonReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,30 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(Register.this, "Wpisz E-mail", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(Register.this, "Wpisz hasło", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
         });
     }
 }
