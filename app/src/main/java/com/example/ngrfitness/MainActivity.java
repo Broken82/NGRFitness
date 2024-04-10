c1fbffaeefddb8d3e170ae05868b421ed3df1543
Maciej Mierzwiak <maciej.mierzwiak@astozi.pl>
2024-04-10 19:25:05 +0200

    Login and Registration

--- app/src/main/java/com/example/ngrfitness/MainActivity.java
+++ app/src/main/java/com/example/ngrfitness/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ngrfitness;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    TextView textView;
+
+    Button buttonLogout;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        buttonLogout = findViewById(R.id.bnt_logout);
+        textView = findViewById(R.id.user_details);
+        currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(currentUser.getEmail());
+        }
+
+        buttonLogout.setOnClickListener(v -> {
+            mAuth.signOut();
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
